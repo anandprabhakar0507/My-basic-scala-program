ae6193cf28d1f0f3ad58e5394e7daff1ab33fcba
Anand prabhakar <anandprabhakar0507@gmail.com>
2018-12-10 23:31:10 +0530

    Create ap.scala

---
+++ ap.scala
@@ -0,0 +1,10 @@
+import fiddle.Fiddle, Fiddle.println
+import scalajs.js
+
+@js.annotation.JSExportTopLevel("ScalaFiddle")
+object ScalaFiddle {
+  // $FiddleStart
+  // Start writing your ScalaFiddle code here
+   println("Hello Friends,This is ANAND PRABHAKAR");
+  // $FiddleEnd
+}
